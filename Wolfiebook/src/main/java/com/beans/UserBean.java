05d325e145828dcfbe5a1c77dbb129a121b04b01
Jeonghoon Kim <jeonghoon.kim@stonybrook.edu>
2016-12-07 19:09:57 -0500

    View & join group in progress

--- Wolfiebook/src/main/java/com/beans/UserBean.java
+++ Wolfiebook/src/main/java/com/beans/UserBean.java
@@ -14,6 +14,7 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
+import javax.servlet.http.HttpSession;
 
 
 /**
